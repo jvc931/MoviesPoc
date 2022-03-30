da2fff14df5c19d9dfd2247091520715c5b52d3d
jonathan-guerrero-a <daynet1980>
2022-03-30 15:41:02 -0500

    Update of HomeItem.kt, HomeScreen.kt and Movies.kt to  use the data from the API

--- app/src/main/java/com/teamb/moviespoc/ui/home/HomeItem.kt
+++ app/src/main/java/com/teamb/moviespoc/ui/home/HomeItem.kt
@@ -5,27 +5,24 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
-import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.teamb.moviespoc.R
+import coil.compose.rememberImagePainter
+import com.teamb.moviespoc.domain.model.PopularMovie
 import com.teamb.moviespoc.ui.theme.MoviesPOCTheme
 
 
 @Composable
-fun HomeItem() {
+fun HomeItem(movie: PopularMovie) {
     Card(
         modifier = Modifier
             .padding(start = 16.dp, end = 16.dp, top = 16.dp, bottom = 8.dp),
@@ -40,8 +37,8 @@ fun HomeItem() {
                     .width(440.dp)
                     .background(Color.Black),
                 contentScale = ContentScale.FillBounds,
-                painter = painterResource(R.mipmap.placeholder),
-                contentDescription = "Movie Preview",
+                painter = rememberImagePainter(data = "https://image.tmdb.org/t/p/original/${movie.posterPath}"),
+                contentDescription = movie.title,
             )
 
 //            Row(
@@ -56,7 +53,7 @@ fun HomeItem() {
 
             Text(modifier = Modifier
                 .padding(top = 8.dp),
-                text = "Title",
+                text = movie.title?:"",
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis,
                 style = MaterialTheme.typography.h5,
@@ -64,8 +61,18 @@ fun HomeItem() {
             )
 
             Text(
-                text = "Description",
-                maxLines = 1,
+                text = movie.overview?:"",
+                textAlign = TextAlign.Justify,
+                maxLines = 5,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.h6,
+                color = Color.Black,
+                modifier = Modifier.padding(bottom = 8.dp)
+            )
+            Text(
+                text = "Score: ${movie.voteAverage}",
+                textAlign = TextAlign.Justify,
+                maxLines = 5,
                 overflow = TextOverflow.Ellipsis,
                 style = MaterialTheme.typography.h6,
                 color = Color.Black,
@@ -79,6 +86,6 @@ fun HomeItem() {
 @Composable
 fun HomeItemPreview() {
     MoviesPOCTheme {
-        HomeItem()
+       // HomeItem()
     }
 }
--- app/src/main/java/com/teamb/moviespoc/ui/view/MainActivity.kt
+++ app/src/main/java/com/teamb/moviespoc/ui/view/MainActivity.kt
@@ -21,11 +21,11 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        popularViewModel.onCreate()
         setContent {
             MoviesPOCTheme {
               SetUpNavGraph(navController = rememberNavController())
             }
         }
-        popularViewModel.onCreate()
     }
 }
